75d0d2a5fb164b4d21a32ca5fdf582362a99905c
Facundo Enriquez <fenriquez@inym.org.ar>
2024-11-29 13:12:03 -0300

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enum\PermissionsEnum;
+use App\Enum\RolesEnum;
+use App\Models\Feature;
+use App\Models\User;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $userRole = Role::create(['name' => RolesEnum::User->value]);
+        $commenterRole = Role::create(['name' => RolesEnum::Commenter->value]);
+        $adminRole = Role::create(['name' => RolesEnum::Admin->value]);
+
+        $manageFeaturesPermission = Permission::create([
+            'name' => PermissionsEnum::ManageFeatures->value
+        ]);
+        $manageUsersPermission = Permission::create([
+            'name' => PermissionsEnum::ManageUsers->value
+        ]);
+        $manageCommentsPermission = Permission::create([
+            'name' => PermissionsEnum::ManageComments->value
+        ]);
+        $upvoteDownvotePermission = Permission::create([
+            'name' => PermissionsEnum::UpvoteDownvote->value
+        ]);
+
+        $userRole->syncPermissions([$upvoteDownvotePermission]);
+        $commenterRole->syncPermissions([$upvoteDownvotePermission, $manageCommentsPermission]);
+        $adminRole->syncPermissions([
+            $manageUsersPermission,
+            $manageCommentsPermission,
+            $upvoteDownvotePermission,
+            $manageFeaturesPermission
+        ]);
+
+        User::factory()->create([
+            'name' => 'User User',
+            'email' => 'user@example.com',
+            'password' => bcrypt('admin123'),
+        ])->assignRole(RolesEnum::User);
+
+        User::factory()->create([
+            'name' => 'Commenter User',
+            'email' => 'commenter@example.com',
+            'password' => bcrypt('admin123')
+        ])->assignRole(RolesEnum::Commenter);
+
+        User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('admin123')
+        ])->assignRole(RolesEnum::Admin);
+
+        Feature::factory(100)->create();
+
+    }
+}
